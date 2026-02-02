75b35f13b098c9e8552797ce56ee23590f7b1d1e
anastasiafoth <anastasiafoth9@gmail.com>
2026-02-02 13:56:36 +0100

    Added all components, written CSS files

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+export default function About () {
+    return (
+      <section className="info">
+        <h2>About</h2>
+        <p className="text">
+          I’m a junior full-stack developer with a background in fashion design,
+          where I spent seven years bringing creative concepts to life in
+          fast-paced, structured projects. My work combined creativity with
+          precision, guiding products from idea to production. I’m excited to
+          grow into a full-stack developer role where I can blend creative
+          thinking with technical problem-solving to build intuitive,
+          well-structured software solutions.
+        </p>
+      </section>
+    );
+}
---
+++ src/components/Interests.jsx
@@ -0,0 +1,12 @@
+export default function Interests () {
+    return (
+      <section className="about">
+        <h2>Interests</h2>
+        <p className="text">
+          Exploring new web technologies. User experience (UX) and interface
+          design. Digital illustration. 3D modeling and digital design. Creative
+          problem solving.
+        </p>
+      </section>
+    );
+}
